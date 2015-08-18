6a0214c3a43f00117961e5dff0c131eb29837909
kawasima <kawasima1016@gmail.com>
2015-08-18 09:54:55 +0900

    Bumped up the version of clojure

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject job-streamer-notificator (clojure.string/trim-newline (slurp "VERSION"))
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [environ "1.0.0"]
                  [ch.qos.logback/logback-classic "1.1.3"]
                  [org.apache.camel/camel-core  "2.15.2"]
@@ -8,7 +8,8 @@
                  [com.github.jknack/handlebars "2.2.3"]]
   :source-paths ["src/clj"]
   :source-java-paths ["src/java"]
-  
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+
   :aot :all
   :main job-streamer.notificator.core
   :pom-plugins [[org.apache.maven.plugins/maven-assembly-plugin "2.5.5"

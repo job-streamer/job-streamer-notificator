eabf9be65e6f4c0def05d5924abb129fde9cdb91
kawasima <kawasima1016@gmail.com>
2016-07-05 22:17:53 +0900

    Bumped up to v0.3.0-SNAPSHOT

--- src/clj/job_streamer/notificator/core.clj
+++ src/clj/job_streamer/notificator/core.clj
@@ -37,10 +37,22 @@
   [(filter (fn [[type config]] (find config :to)) rules)
    (filter (fn [[type config]] (not (find config :to))) rules)])
 
+(def banner "
+   ___       _     _____ _
+  |_  |     | |   /  ___| |                         Notificator
+    | | ___ | |__ \ `--.| |_ _ __ ___  __ _ _ __ ___   ___ _ __
+    | |/ _ \| '_ \ `--. \ __| '__/ _ \/ _` | '_ ` _ \ / _ \ '__|
+/\__/ / (_) | |_) /\__/ / |_| | |  __/ (_| | | | | | |  __/ |
+\____/ \___/|_.__/\____/ \__|_|  \___|\__,_|_| |_| |_|\___|_|
+")
+
 (defn -main [& args]
+  (when (empty? args)
+    (.println *err* "Usage: bin/notificator [rule file]")
+    (.exit (Runtime/getRuntime) 255))
   (let [rules   (load-file (first args))
         context (DefaultCamelContext.)
-        port (get env :port 2121)
+        port (Integer/parseInt (or (:notificator-port env) "2121"))
         [consumer-rules producer-rules] (filter-rules rules)]
     (.addRoutes context
                 (proxy [RouteBuilder] []

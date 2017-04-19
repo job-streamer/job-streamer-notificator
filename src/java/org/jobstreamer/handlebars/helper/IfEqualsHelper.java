7e748f53526d6a76f41ea9985d482ac708569443
223kazuki <rixi223.kazuki@gmail.com>
2017-04-19 23:19:58 +0900

    Added IfEqualsHandler.

---
+++ src/java/org/jobstreamer/handlebars/helper/IfEqualsHelper.java
@@ -0,0 +1,41 @@
+package org.jobstreamer.handlebars.helper;
+
+import java.io.IOException;
+
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Options.Buffer;
+
+/**
+ * You can use the ifequals helper to conditionally render a block. If its first
+ * argument and second argument are not equal, Handlebars will not render the block.
+ *
+ * @author tsutusmi kazuki
+ * @since 1.0.1
+ */
+public class IfEqualsHelper implements Helper<Object> {
+
+  /**
+   * A singleton instance of this helper.
+   */
+  public static final Helper<Object> INSTANCE = new IfEqualsHelper();
+
+  /**
+   * The helper's name.
+   */
+  public static final String NAME = "ifequals";
+
+  @Override
+  public CharSequence apply(final Object context, final Options options)
+      throws IOException {
+    Buffer buffer = options.buffer();
+    String target = (String) options.param(0, null);
+
+    if ( target == null || !target.equals(context)) {
+      buffer.append(options.inverse());
+    } else {
+      buffer.append(options.fn());
+    }
+    return buffer;
+  }
+}

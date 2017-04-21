40e57f1d7916fb6e622d2f0dc7aa8ec452440757
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2017-04-21 10:03:02 +0900

    Modified bat file to get command line arguments.

--- bin/notificator.bat
+++ bin/notificator.bat
@@ -4,6 +4,6 @@ pushd %0\..\..
 
 set /p VERSION=<VERSION
 
-java -cp dist\job-streamer-notificator-%VERSION%.jar;"lib\*" clojure.main -m job-streamer.notificator.main
+java -cp dist\job-streamer-notificator-%VERSION%.jar;"lib\*" clojure.main -m job-streamer.notificator.main %*
 
 pause
